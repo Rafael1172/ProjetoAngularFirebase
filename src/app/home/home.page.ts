fa4e39c1c68de85fe58012e0fb31f99e1010d1b1
DiogoPCS <diogo.paulino.santos@hotmail.com>
2025-06-06 16:28:56 -0300

    teste

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,17 +12,28 @@ import { Router } from '@angular/router';
 })
 export class HomePage {
 
-  pokemon:any = {
-    nome: null,
-    poder: null
-  };
-
-  constructor( 
-    public crudService: CrudService
-  ){ }
-
-  enviar() {
-    this.crudService.insert(this.pokemon, 'pokemons');
-  }
+    side_menu: any = [
+      {
+        icon: 'home-outline',
+        name: 'Página Inicial',
+        selected: true
+      },
+      {
+        icon: 'cube-outline',
+        name: 'Produtos',
+        selected: false
+      },
+      {
+        icon: 'people-outline',
+        name: 'Clientes',
+        selected: false
+      },
+      {
+        icon: 'call-online',
+        name: 'Contato',
+        selected: false
+      },
+      
+    ];
 
 }
